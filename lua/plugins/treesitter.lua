a6c96013ed16d5288f9147143c4da0b4f27c156d
Gibbs19 <7484829+gibbs19@users.noreply.github.com>
2024-04-24 23:35:11 -0400

    lsp conf

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config=function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "ruby","c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      ensure_installed = { "ruby","c", "lua", "vim", "vimdoc", "query", "elixir","javascript", "html" },
       highlight = { enable = true },
       indent = { enable = true },  
     }
