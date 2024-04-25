c962aff25749fc8525cd31d0859b5cfea878af5f
Gibbs19 <7484829+gibbs19@users.noreply.github.com>
2024-04-24 22:12:27 -0400

    Update

--- init.lua
+++ init.lua
@@ -11,7 +11,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
-vim.opt.clipboard = "unnamedplus"
 local opts = {}
 
 require("vim-options")
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,6 +1,9 @@
+vim.g.mapleader = " "
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=1")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
 vim.cmd("set clipboard+=unnamedplus")
+vim.cmd("map <S-j> <C-d>")
+vim.cmd("map <S-k> <C-u>")
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
